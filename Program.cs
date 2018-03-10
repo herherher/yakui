e3b8563e18bb9a7abc6c0cec3264df240969b6b8
Yakui <37144819+yakui-san@users.noreply.github.com>
2018-03-10 13:12:17 +0100

    Version version. 0.0.1(Does nothing to be honest)

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace Yakui_the_maid
+{
+    class Program
+    {
+        static DiscordClient discord;
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            string prefix = "??";
+
+            string[] config = System.IO.File.ReadAllLines(Directory.GetCurrentDirectory() + "\\token1.txt");
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = config[0],
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+
+            });
+
+            discord.MessageCreated += async e =>
+            {
+                string[] commands = new string[] {"help"};
+                foreach(string command in commands){
+                    if(e.Message.Content.StartsWith(prefix + command) && command == "help")
+                    {
+                        await e.Message.RespondAsync("Here is a list of the currently available commands:\n??help: prints out this list.\n??idk");
+                    }
+                }
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
